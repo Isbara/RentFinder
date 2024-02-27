d3f056324fcf9455f7b813c5033593746c5ef706
Isbara <isbaralpyildirim@gmail.com>
2024-02-27 22:13:33 +0200

    Initial commit

---
+++ src/main/java/GradProject/RentFinder/Repository/UserRepository.java
@@ -0,0 +1,15 @@
+package GradProject.RentFinder.Repository;
+
+import GradProject.RentFinder.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    @Query(nativeQuery = true, value = "SELECT * FROM COMPANY_TABLE WHERE USERNAME = :username")
+    Optional<User> findByUsername(@Param("username") String username);
+
+    void deleteByUsername(String username);
+}
---
+++ src/main/java/GradProject/RentFinder/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,37 @@
+package GradProject.RentFinder.SecurityConfig;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter JwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/company/login", "/company/register", "/h2-console", "/h2-console/**").permitAll()
+                        .anyRequest().authenticated())
+                .headers((headerauth) -> headerauth
+                        .frameOptions((frameopt) -> frameopt.disable()))
+                .sessionManagement((s) -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(JwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
